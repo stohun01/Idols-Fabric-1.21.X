3a1f2a2360adf7e5c7e98efc9d77960db46baf0c
stohun01 <stohun01@gmail.com>
2025-09-22 16:06:44 +1000

    Initial commit

---
+++ src/main/java/net/stohun/idols/IdolsClient.java
@@ -0,0 +1,10 @@
+package net.stohun.idols;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IdolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/stohun/idols/IdolsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.stohun.idols;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IdolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
